347bbddb2260190a45b1140b8276815adae31893
ap <andras.petocz@gmail.com>
2025-12-02 16:39:23 +0100

    fix of fix

--- terra_futura/interfaces.py
+++ terra_futura/interfaces.py
@@ -194,7 +194,10 @@ class ProcessActionAssistanceInterface(Protocol):
 
 
 class InterfaceSelectReward(Protocol):
-    player: int
+    @property
+    def player(self) -> int:
+        ...
+    
     def setReward(self, player: int, card: InterfaceCard, reward: List[Resource]) ->None:
         ...
     
